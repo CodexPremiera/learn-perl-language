8158382a8529d4078979db4027b1ffd437179d58
CodexPremiera <ashleyken.professional@gmail.com>
2025-01-17 15:17:10 +0800

    lesson01: learn the boilerplate, comments, and printing in PERL

---
+++ lesson01_hello_world/helloworld.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+# This is the shebang line that tells the system to use Perl to run this script.
+
+use strict;     # enforce strict variable declaration
+use warnings;   # enable warnings to catch common mistakes
+
+# Print "Hello, World!" to the console
+print "Hello, World!
+This is my first PERL code.
+"
+
+# Comments are single-lines that starts with `#`
+
+# To run perl program, type `perl filename.pl` in the terminal
